6dd175322978b89190c0001fe715f905a9cb4c46
Ethan Salonga <ethansalonga28@gmail.com>
2023-03-18 15:42:31 -0400

    Build checklist page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import "./styles/App.css"
+import top100AnimeArr from "./data/top100anime"
+
 function App() {
-  return <div>Hello world</div>
+  return (
+    <>
+      <main>
+        <h1 className="header">Top 100 Anime Checklist</h1>
+        <div className="widthContainer">
+          <section className="animeCards">
+            {top100AnimeArr.map((item) => (
+              <div className="animeItem">
+                <div className="animeTitle">{item.title}</div>
+                <div className="animeCard"></div>
+              </div>
+            ))}
+          </section>
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default App
